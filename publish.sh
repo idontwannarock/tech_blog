bbd372e8c99460acb7d4d0122282ee744dfcc36c
Howard Wang <idontwannarock@gmail.com>
2021-11-03 16:20:32 +0800

    ci: fix publish.sh

--- publish.sh
+++ publish.sh
@@ -26,7 +26,7 @@ echo "Generating site"
 hugo
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "feat: publish to gh-pages (publish.sh)" & cd ..
+cd public && git add --all && git commit -m "feat: publish to gh-pages (publish.sh)" && cd ..
 
 # publish
 git push origin gh-pages
